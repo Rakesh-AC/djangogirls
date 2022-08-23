11b6b4979f95d78726a177e00c9f95713e968cb3
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-23 12:58:01 +0530

    Update ngnix.sh

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -1,7 +1,7 @@
 
-sudo vim /etc/nginx/sites-available/codeX
+sudo vim /etc/nginx/sites-available/djangogirls
 
-sudo cp /home/ubuntu/djangogirls/nginx/codex.conf /etc/nginx/sites-available/codeX
+sudo cp /home/ubuntu/djangogirls/nginx/codex.conf /etc/nginx/sites-available/djangogirls
 
 sudo ln -s /etc/nginx/sites-available/djangogirls /etc/nginx/sites-enabled/
 
