c00e775510202a68faa9b4b8e067345f630b7512
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-23 12:54:15 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,6 +1,6 @@
 
-virtualenv /home/ubuntu/codeX/codexenv
-source /home/ubuntu/codeX/codexenv/bin/activate
-pip3 install -r /home/ubuntu/codeX/requirements.txt
-touch /home/ubuntu/codeX/.env
-cp /home/ubuntu/codeX/.cirleci/env /home/ubuntu/codeX/.env
+virtualenv /home/ubuntu/myp/djangogirls/djenv
+source /home/ubuntu/myp/djangogirls/djenv/bin/activate
+pip3 install -r /home/ubuntu/myp/djangogirls/requirements.txt
+touch /home/ubuntu/myp/djangogirls/.env
+cp /home/ubuntu/myp/djangogirls/.cirleci/env /home/ubuntu/myp/djangogirls/.env
