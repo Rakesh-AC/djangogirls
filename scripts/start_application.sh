efb21f9225657488a8ae716310f9c688683aa1f9
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-23 13:01:11 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,6 @@
 
-cd /home/ubuntu/codeX/
-source /home/ubuntu/codeX/codexenv/bin/activate
+cd /home/ubuntu/myp/djangogirls
+source /home/ubuntu/myp/djangogirls/djenv/bin/activate
 python3 manage.py collectstatic
 sudo systemctl start gunicorn
 sudo systemctl start nginx
