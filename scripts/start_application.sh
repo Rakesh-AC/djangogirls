4da22ad4b2a0c914491bedea7fc2eb3a02beac48
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-24 17:53:14 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,6 @@
 
-cd /home/ubuntu/myp/djangogirls
-source /home/ubuntu/myp/djangogirls/djenv/bin/activate
+cd /home/ubuntu/djangogirls
+source /home/ubuntu/djangogirls/djenv/bin/activate
 python3 manage.py collectstatic
 sudo systemctl start gunicorn
 sudo systemctl start nginx
