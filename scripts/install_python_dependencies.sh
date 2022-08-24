598d13e2637bbc906bac1a4fa65eeade796d494f
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-24 19:57:05 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -2,5 +2,4 @@
 virtualenv /home/ubuntu/djangogirls/djenv
 source /home/ubuntu/djangogirls/djenv/bin/activate
 pip3 install -r /home/ubuntu/djangogirls/requirements.txt
-touch /home/ubuntu/djangogirls/.env
-cp /home/ubuntu/djangogirls/.cirleci/env /home/ubuntu/djangogirls/.env
+
