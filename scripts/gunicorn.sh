29f566254fff0516acda5592b64ee90a6b07f4f2
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-30 13:08:02 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -3,6 +3,5 @@ sudo touch /etc/systemd/system/gunicorn.service
 
 sudo cp /home/ubuntu/myprojectdir/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
 sudo cp /home/ubuntu/myprojectdir/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
-
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
