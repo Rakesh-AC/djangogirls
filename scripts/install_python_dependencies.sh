6d713bc7c94cc4236acfb48206a3eac8ca39cd55
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-30 12:06:49 +0530

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,5 +1,20 @@
 
-virtualenv /home/ubuntu/djangogirls/djenv
-source /home/ubuntu/djangogirls/djenv/bin/activate
-pip3 install -r /home/ubuntu/djangogirls/requirements.txt
+sudo -H pip3 install --upgrade pip
 
+sudo -H pip3 install virtualenv
+
+cd ~/myprojectdir
+virtualenv myprojectenv
+source myprojectenv/bin/activate
+pip install django gunicorn psycopg2-binary
+pip3 install -r /home/ubuntu/myprojectdir/requirements.txt
+
+~/myprojectdir/manage.py makemigrations
+
+~/myprojectdir/manage.py migrate
+
+~/myprojectdir/manage.py collectstatic
+
+sudo ufw allow 8000
+
+~/myprojectdir/manage.py runserver 0.0.0.0:8000
