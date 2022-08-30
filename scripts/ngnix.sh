f5015c2366bb3c397f6b4908a7d47b8bb43836fb
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-30 21:51:45 +0530

    Update ngnix.sh

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -3,7 +3,7 @@ sudo vim /etc/nginx/sites-available/djangogirls
 
 sudo cp /home/ubuntu/myprojectdir/nginx/djangogirls.conf /etc/nginx/sites-available/djangogirls
 
-sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/myprojectdir /etc/nginx/sites-enabled
 
 sudo rm -rf /etc/nginx/sites-available/default.conf
 
