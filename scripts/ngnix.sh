25794adcfdea49533710ae3d7f725f281281b3a8
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-30 12:43:22 +0530

    Update ngnix.sh

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -1,10 +1,14 @@
 
 sudo vim /etc/nginx/sites-available/djangogirls
 
-sudo cp /home/ubuntu/djangogirls/nginx/codex.conf /etc/nginx/sites-available/djangogirls
+sudo cp /home/ubuntu/myprojectdir/djangogirls/nginx/codex.conf /etc/nginx/sites-available/djangogirls
 
 sudo ln -s /etc/nginx/sites-available/djangogirls /etc/nginx/sites-enabled/
 
 sudo rm -rf /etc/nginx/sites-available/default.conf
 
 sudo systemctl restart nginx
+
+sudo ufw delete allow 8000
+
+sudo ufw allow 'Nginx Full'
