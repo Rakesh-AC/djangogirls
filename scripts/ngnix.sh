1ffc38341726f1df48caad0b32cd95e6effca770
vamshi-smallday <99802380+vamshi-smallday@users.noreply.github.com>
2022-08-30 22:34:16 +0530

    Update ngnix.sh

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -3,7 +3,7 @@ touch /etc/nginx/sites-available/djangogirls
 
 sudo cp /home/ubuntu/myprojectdir/nginx/codex.conf /etc/nginx/sites-available/djangogirls
 
-sudo ln -s /etc/nginx/sites-available/myprojectdir /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/djangogirls /etc/nginx/sites-enabled
 
 sudo rm -rf /etc/nginx/sites-available/default
 
