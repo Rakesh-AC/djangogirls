30b455ec699d2cec858042eae1b0eb7adb05d5f2
RakeshAC <97755979+Rakesh-AC@users.noreply.github.com>
2022-08-31 11:12:32 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = {"post 1":"first post", "post 2":"second post"}
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
